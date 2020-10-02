fd36eee146461d00b19fedfcd7656783de596bab
Alon Ravid <alonri75@walla.com>
2020-10-02 12:47:27 +0300

    Initial Commit

---
+++ Twitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter
+//
+//  Created by Alon Ribovsky on 02/10/2020.
+//  Copyright © 2020 Alon Ribovsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
